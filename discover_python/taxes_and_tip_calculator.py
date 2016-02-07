e0fbeda997676fc781d583426a59b3507d471e4f
Asaia Palacios <asaia.palacios@gmail.com>
2016-02-07 11:26:51 -0800

    edit comments to clarify action taken: prompts for user input; why use float function to convert string; include price with exactly 6 numbers after decimal.

--- discover_python/taxes_and_tip_calculator.py
+++ discover_python/taxes_and_tip_calculator.py
@@ -2,12 +2,15 @@
 
 print "Welcome to the taxes and tip calculator!"
 
-# Using float input function
+# Prompts for user input
+# Use float function to convert input statement string to a real number or "float" in python
 meal = float(input("What is the price before tax?"))
 tax = float(input("What are the taxes? (in %)"))
 tip = float(input("What do you want to tip? (in %)"))
 
 total = (meal+(meal*tax/100))*(1+(tip/100))
+
+# Include price with exactly 6 numbers after the decimal
 print("The price you need to pay is: $ %.6f.")% total
 
 
