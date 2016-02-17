4f558940e3c6cc95f92cfd02d893c500396bbf5c
Asaia Palacios <asaia.palacios@gmail.com>
2016-02-16 16:15:33 -0800

    Update write_them_to_disc.js
    Saving string to a file: adding comments

--- discover_javascript/write_them_to_disc.js
+++ discover_javascript/write_them_to_disc.js
@@ -5,28 +5,33 @@ var options = {
     hostname: 'api.github.com',
     path: '/search/repositories?q=language:javascript&sort=stars&order=desc',
     headers: {
-	'User-Agent': 'Holberton_School',
-	'Authorization': 'token 149185fe4572831c683f826350f65387fa9777aa'
+        'User-Agent': 'Holberton_School',
+        'Authorization': 'token 149185fe4572831c683f826350f65387fa9777aa'
     }
 }
 
 function streamToString(stream, cb) {
     const chunks = [];
     stream.on('data', (chunk) => {
-	chunks.push(chunk);
+        chunks.push(chunk);
     });
     stream.on('end', () => {
-	cb(chunks.join(''));
+        cb(chunks.join(''));
     });
 }
 
 var req = https.request(options, (res) => {
     streamToString(res, function(jsonString) {
-	console.log(typeof jsonString);
-	console.log(jsonString);
+        // console.log(typeof jsonString); //                                                     
+        fs.writeFile("/tmp/17", jsonString, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+        console.log("The file was saved!");
+        });
     });
 });
-req.end();
+    req.end();
 
 req.on('error', function(e) {
     console.error(e);
