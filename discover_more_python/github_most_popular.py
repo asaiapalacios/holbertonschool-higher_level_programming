827ba8e061582d8632f903742cfb868cab77790f
Daniel Alzugaray <dalzuga@gmail.com>
2016-04-03 13:58:56 -0700

    Calling github url using Python. Task 0 & 1

---
+++ discover_more_python/github_most_popular.py
@@ -0,0 +1,12 @@
+import urllib2
+
+request_headers = {
+  'User-Agent': 'Holberton_School',
+  'Authorization': 'token 27aa992fd7186938f589f8b6da49a7264dcc808a'
+}
+
+githuburl = 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc'
+
+req = urllib2.Request(githuburl, headers=request_headers)
+response = urllib2.urlopen(req)
+print response.read()
---
+++ discover_more_python/write_them_to_disc.py
@@ -0,0 +1,26 @@
+import urllib2
+import os.path
+
+request_headers = {
+  'User-Agent': 'Holberton_School',
+  'Authorization': 'token 27aa992fd7186938f589f8b6da49a7264dcc808a'
+}
+
+githuburl = 'https://api.github.com/search/repositories?q=language:python&sort=\
+stars&order=desc'
+req = urllib2.Request(githuburl, headers=request_headers)
+response = urllib2.urlopen(req)
+
+# print response.read()
+
+if os.path.exists("/tmp/17"):
+    f = open("/tmp/17", "w")
+    f.truncate()
+    f.close()
+
+varfile = open("/tmp/17", "w")
+
+varfile.write(response.read())
+varfile.close()
+print "The file was saved!"  
+
