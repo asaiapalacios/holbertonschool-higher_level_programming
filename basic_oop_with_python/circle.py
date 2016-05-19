1c0b1846cbe8baf30e4fc09fcebe3cadb1445852
Asaia Palacios <asaia.palacios@gmail.com>
2016-05-18 22:16:05 -0700

    Circle class python script using private attrib, public attrib, and public method

--- basic_oop_with_python/circle.py
+++ basic_oop_with_python/circle.py
@@ -1,4 +1,42 @@
 '''
-Script circle.py describes a Circle w/specific requisites
+Script circle.py describes a Circle Class w/specific specs: private attributes, public attributes & public method
 '''
+import math
 
+class Circle:
+    #Constructor
+    def __init__(self, radius):
+        self.__radius = radius
+        self.name = "circle"
+
+    #Destructor
+    def __del__(self):
+        pass
+
+    #Getter for color
+    def get_color(self):
+        return self.__color
+
+    #Setter for color
+    def set_color(self, color):
+        self.__color = color
+
+    #Getter for radius
+    def get_radius(self):
+        return self.__radius
+
+    #Setter for radius
+    def set_radius(self, radius):
+        self.__radius = radius
+    
+    #Setter for center
+    def set_center(self, center):
+        self.__center = [0, 0]
+
+    #Getter for center
+    def get_center(self):
+        return self.__center
+
+    #Find and return area
+    def area(self):
+        return self.__radius * self.__radius * math.pi
