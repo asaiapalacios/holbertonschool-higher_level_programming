bd5cf604ccc37cb954d759f1c208a2b513ba012f
Asaia Palacios <asaia.palacios@gmail.com>
2016-06-02 17:58:24 -0700

    Reading XML file: Task 0

---
+++ Table View/Table View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Table View
+//
+//  Created by Asaia Palacios on 5/27/16.
+//  Copyright © 2016 Asaia Palacios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Tapper_Test/Tapper_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tapper_Test
+//
+//  Created by Asaia Palacios on 5/27/16.
+//  Copyright © 2016 Asaia Palacios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
